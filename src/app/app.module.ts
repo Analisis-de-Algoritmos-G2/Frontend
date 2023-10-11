0d0450b14b5ebf9950d250c9eca13fa421ef064b
Angie Tatiana Peña <penap_at@javeriana.edu.co>
2023-10-11 15:38:00 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RulesComponent } from './rules/rules.component';
 import { FightComponent } from './fight/fight.component';
 import { TextCarouselComponent } from './text-carousel/text-carousel.component';
+import { PhotoCarouselComponent } from './photo-carousel/photo-carousel.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RulesComponent,
     FightComponent,
-    TextCarouselComponent
+    TextCarouselComponent,
+    PhotoCarouselComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
